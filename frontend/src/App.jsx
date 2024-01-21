a5bcbb1395f19184992941a4910c35f3fff1ca78
Oyetade Segun John <johnwellacademy@gmail.com>
2024-01-21 19:29:32 +0100

    All done with

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,11 @@ import './App.css'
 import './pages/Home'
 import Home from './pages/Home'
 import { Routes, Route } from 'react-router-dom'
-// import SwaggerDocs from './pages/SwaggerDocs'
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<Home/>} />
-      {/* <Route path= "/api-docs" element= {<SwaggerDocs/>}/> */}
     </Routes>
 
   )
--- frontend/src/pages/SwaggerDocs.jsx
+++
@@ -1,9 +0,0 @@
-// import React from 'react'
-
-// function SwaggerDocs() {
-//   return (
-//     <div>SwaggerDocs</div>
-//   )
-// }
-
-// export default SwaggerDocs

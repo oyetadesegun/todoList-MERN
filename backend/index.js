cee560e5b4974f133875d2540a3025dc07b55511
Oyetade Segun John <johnwellacademy@gmail.com>
2024-01-21 18:41:04 +0100

    All done with

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,31 @@
-const express = require('express')
-require('dotenv').config()
-const mongoose = require('mongoose')
-const cors = require('cors')
-const routes = require('./Routers/todoRoutes')
-const app = express()
-app.use(cors({
-    origin:["http://localhost:3001",]
-}))
-app.use(express.json())
+const express = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const routes = require("./Routers/todoRoutes");
+const app = express();
+app.use(
+  cors({
+    origin: ["http://localhost:3001", "http://localhost:5173"],
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+app.use(express.json());
 // Swagger Documentation
-const swaggerUI = require('swagger-ui-express')
-const YAML = require('yamljs')
-const swaggerJsDocs = YAML.load('./api.yaml')
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerJsDocs = YAML.load("./api.yaml");
 
-
-mongoose.connect(process.env.MONGODB_URI
-).then(()=>console.log("Connected to MongoDB..."))
-.catch((err)=>console.log(err))
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.log(err));
 
 //MIDDLEWARE
-app.use(routes)
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerJsDocs))
+app.use(routes);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerJsDocs));
 
-app.listen(3001, ()=>{
-    console.log("Server is running on port")
-})
+app.listen(3001, () => {
+  console.log("Server is running on port");
+});
